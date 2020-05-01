bdb21f7900473dfb8a5300c7d6fe5e70584adaa5
Orlin Genchev <ogenchev@gmail.com>
2020-05-02 00:38:19 +0300

    comments

--- src/main/java/one/genchev/primes/util/PrimeChecker.java
+++ src/main/java/one/genchev/primes/util/PrimeChecker.java
@@ -2,9 +2,17 @@ package one.genchev.primes.util;
 
 import org.springframework.stereotype.Component;
 
+/**
+ * Methods for determining prime numbers
+ */
 @Component
 public class PrimeChecker {
 
+    /**
+     * Check if input number is prime
+     * @param input
+     * @return
+     */
     public boolean isPrime(Integer input) {
         if(input % 2 == 0)
             return false;
@@ -19,6 +27,11 @@ public class PrimeChecker {
         return true;
     }
 
+    /**
+     * Return the closes prime number, bigger than the input number
+     * @param input
+     * @return
+     */
     public Integer findNextPrime(Integer input) {
         Integer result = input+1;
         while(result % 2 == 0 || !isPrime(result)) {
