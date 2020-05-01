814c0ace5e5d1f8d185c1b06e067b486e58a5dad
Orlin Genchev <ogenchev@gmail.com>
2020-05-01 13:34:31 +0300

    initial project base commit

---
+++ src/test/java/one/genchev/primes/PrimesApplicationTests.java
@@ -0,0 +1,13 @@
+package one.genchev.primes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
