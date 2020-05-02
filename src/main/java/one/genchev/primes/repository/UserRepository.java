21f8476dd0334c59a2a7c53cbc116417aa77f4e2
Orlin Genchev <ogenchev@gmail.com>
2020-05-02 23:30:55 +0300

    remove user management

--- src/main/java/one/genchev/primes/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package one.genchev.primes.repository;
-
-import one.genchev.primes.model.User;
-
-@Deprecated
-public interface UserRepository /*extends MongoRepository<User, String>*/ {
-
-    User findByEmail(String email);
-}
